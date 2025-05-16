ee6f09f68255840316ad58b04eae054603c43a34
Vitaliy Patrikeev <vppatric@gmail.com>
2025-05-17 00:07:11 +0300

    add version help

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,20 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import java.util.concurrent.Callable;
+
+@Command(
+        name = "gendiff",
+        mixinStandardHelpOptions = true,
+        description = "Compares two configuration files and shows a difference."
+)
+public class App implements Runnable {
     public static void main(String[] args) {
+        new CommandLine(new App()).execute(args);
+    }
+    @Override
+    public void run() {
         System.out.println("Hello World!");
     }
 }
