86f677710621353071b4a1a8855c5f50df141fb7
Vitaliy Patrikeev <vppatric@gmail.com>
2025-06-13 21:24:00 +0300

    update Differ class

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.StringJoiner;
 
 public class Differ {
     public static String generate(Map<String, String> firstMapOfJson, Map<String, String> secondMapOfJson) {
@@ -14,24 +15,22 @@ public class Differ {
         List<String> sortedSetOfKeys = new ArrayList<>(setOfKeys);
         Collections.sort(sortedSetOfKeys);
 
-        StringBuilder result = new StringBuilder();
+        StringJoiner result = new StringJoiner("\n");
+        result.add("{");
         for (var key : sortedSetOfKeys) {
             var result1 = firstMapOfJson.get(key);
             var result2 = secondMapOfJson.get(key);
             if (result1 == null) {
-                result.append("+ ").append(key).append(": ").append(result2).append("\n");
+                result.add("  + " + key + ": " + result2);
             } else if (result2 == null) {
-                result.append("- ").append(key).append(": ").append(result1).append("\n");
+                result.add("  - " + key + ": " + result1);
             } else if (result1.equals(result2)) {
-                result.append(key).append(": ").append(result1).append("\n");
+                result.add("    " + key + ": " + result1);
             } else {
-                result.append("- ").append(key).append(": ")
-                        .append(result1).append("\n")
-                        .append("+ ").append(key).append(": ")
-                        .append(result2).append("\n");
+                result.add("  - " + key + ": " + result1);
+                result.add("  + " + key + ": " + result2);
             }
         }
-
-        return result.toString();
+        return result.add("}").toString();
     }
 }
