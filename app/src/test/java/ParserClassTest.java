ec7b882168451d48aac5b2cd8456354627bb4ebe
Vitaliy Patrikeev <vppatric@gmail.com>
2025-06-14 20:35:50 +0300

    new test class

---
+++ app/src/test/java/ParserClassTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import hexlet.code.Parser;
+
+public class ParserClassTest {
+    @Test
+    // Тест парсинга несуществующего файла
+    public void testParserNoFile() throws Exception {
+        Exception expected = new Exception ("File '/home/vitaliy/java-project-71/app/src/test/resources/nofile.json' does not exist");
+        try {
+            var result = Parser.getData("src/test/resources/nofile.json");
+        } catch (Exception actual) {
+            assertEquals(expected.getMessage(), actual.getMessage());
+        }
+    }
+}
