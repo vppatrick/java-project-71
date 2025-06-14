6d1b6c028ae1a9bdef6f77ed143747a0c776f902
Vitaliy Patrikeev <vppatric@gmail.com>
2025-06-14 20:45:01 +0300

    fix checkstyleTest FAILED

--- app/src/test/java/ParserClassTest.java
+++ app/src/test/java/ParserClassTest.java
@@ -6,7 +6,8 @@ public class ParserClassTest {
     @Test
     // Тест парсинга несуществующего файла
     public void testParserNoFile() throws Exception {
-        Exception expected = new Exception ("File '/home/vitaliy/java-project-71/app/src/test/resources/nofile.json' does not exist");
+        Exception expected = new Exception("File '/home/vitaliy/java-project-71/"
+                + "app/src/test/resources/nofile.json' does not exist");
         try {
             var result = Parser.getData("src/test/resources/nofile.json");
         } catch (Exception actual) {
