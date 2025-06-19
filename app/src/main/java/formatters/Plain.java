2b5c684f49bce72326e5fbae79e96f42ff0b02d3
Vitaliy Patrikeev <vppatric@gmail.com>
2025-06-19 19:05:37 +0300

    add new class for Formatter package

---
+++ app/src/main/java/formatters/Plain.java
@@ -0,0 +1,35 @@
+package formatters;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.StringJoiner;
+
+public class Plain {
+    public static String getFormat(LinkedHashMap<String, HashMap<String, Object>> data) {
+        StringJoiner result = new StringJoiner("\n");
+        for (var key : data.keySet()) {
+            var value = data.get(key);
+            if (value.get("state").equals("added")) {
+                result.add("Property '" + key + "' was added with value: "
+                        + castingToType(value.get("value")));
+            } else if (value.get("state").equals("removed")) {
+                result.add("Property '" + key + "' was removed");
+            } else if (value.get("state").equals("updated")) {
+                result.add("Property '" + key + "' was updated. From "
+                        + castingToType(value.get("oldValue")) + " to "
+                        + castingToType(value.get("newValue")));
+            }
+        }
+        return result.toString();
+    }
+
+    private static String castingToType(Object value) {
+        if (value instanceof Number || value instanceof Boolean || value == null) {
+            return String.valueOf(value);
+        } else if (value instanceof String) {
+            return "'" + value + "'";
+        } else {
+            return "[complex value]";
+        }
+    }
+}
