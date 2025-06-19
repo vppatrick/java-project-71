8ee8ed6518a56fbcb308508bffbe2dc34e103a1a
Vitaliy Patrikeev <vppatric@gmail.com>
2025-06-20 00:39:05 +0300

    update Differ class

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,7 +1,6 @@
 package hexlet.code;
 
 import java.util.Map;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -17,27 +16,27 @@ public class Differ {
         List<String> sortedSetOfKeys = new ArrayList<>(setOfKeys);
         Collections.sort(sortedSetOfKeys);
 
-        LinkedHashMap<String, HashMap<String, Object>> result = new LinkedHashMap<>();
+        LinkedHashMap<String, LinkedHashMap<String, Object>> result = new LinkedHashMap<>();
         for (var key : sortedSetOfKeys) {
             var result1 = firstMapOfData.getOrDefault(key, "absent");
             var result2 = secondMapOfData.getOrDefault(key, "absent");
             if (String.valueOf(result1).equals("absent")) { // added
-                result.put(key, new HashMap<String, Object>() {{
+                result.put(key, new LinkedHashMap<String, Object>() {{
                         put("state", "added");
                         put("value", result2);
                     }});
             } else if (String.valueOf(result2).equals("absent")) { // removed
-                result.put(key, new HashMap<String, Object>() {{
+                result.put(key, new LinkedHashMap<String, Object>() {{
                         put("state", "removed");
                         put("value", result1);
                     }});
             } else if (String.valueOf(result1).equals(String.valueOf(result2))) { // no change
-                result.put(key, new HashMap<String, Object>() {{
+                result.put(key, new LinkedHashMap<String, Object>() {{
                         put("state", "noChange");
                         put("value", result1);
                     }});
             } else { // updated
-                result.put(key, new HashMap<String, Object>() {{
+                result.put(key, new LinkedHashMap<String, Object>() {{
                         put("state", "updated");
                         put("oldValue", result1);
                         put("newValue", result2);
