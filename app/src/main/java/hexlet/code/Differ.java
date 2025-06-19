53ab9ca92c0d48f8d6b68e1c4ed0a80504698a7f
Vitaliy Patrikeev <vppatric@gmail.com>
2025-06-19 13:54:53 +0300

    update Differ class

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,36 +1,49 @@
 package hexlet.code;
 
 import java.util.Map;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.StringJoiner;
 
 public class Differ {
-    public static String generate(Map<String, String> firstMapOfJson, Map<String, String> secondMapOfJson) {
+    public static String generate(Map<String, Object> firstMapOfData,
+                                  Map<String, Object> secondMapOfData, String format) {
         var setOfKeys = new HashSet<String>();
-        setOfKeys.addAll(firstMapOfJson.keySet());
-        setOfKeys.addAll(secondMapOfJson.keySet());
+        setOfKeys.addAll(firstMapOfData.keySet());
+        setOfKeys.addAll(secondMapOfData.keySet());
         List<String> sortedSetOfKeys = new ArrayList<>(setOfKeys);
         Collections.sort(sortedSetOfKeys);
 
-        StringJoiner result = new StringJoiner("\n");
-        result.add("{");
+        LinkedHashMap<String, HashMap<String, String>> result = new LinkedHashMap<>();
         for (var key : sortedSetOfKeys) {
-            var result1 = firstMapOfJson.get(key);
-            var result2 = secondMapOfJson.get(key);
-            if (result1 == null) {
-                result.add("  + " + key + ": " + result2);
-            } else if (result2 == null) {
-                result.add("  - " + key + ": " + result1);
-            } else if (result1.equals(result2)) {
-                result.add("    " + key + ": " + result1);
-            } else {
-                result.add("  - " + key + ": " + result1);
-                result.add("  + " + key + ": " + result2);
+            var result1 = String.valueOf(firstMapOfData.getOrDefault(key, "absent"));
+            var result2 = String.valueOf(secondMapOfData.getOrDefault(key, "absent"));
+            if (result1.equals("absent")) { // added
+                result.put(key, new HashMap<String, String>() {{
+                        put("state", "added");
+                        put("value", result2);
+                    }});
+            } else if (result2.equals("absent")) { // removed
+                result.put(key, new HashMap<String, String>() {{
+                        put("state", "removed");
+                        put("value", result1);
+                    }});
+            } else if (result1.equals(result2)) { // no change
+                result.put(key, new HashMap<String, String>() {{
+                        put("state", "noChange");
+                        put("value", result1);
+                    }});
+            } else { // updated
+                result.put(key, new HashMap<String, String>() {{
+                        put("state", "updated");
+                        put("oldValue", result1);
+                        put("newValue", result2);
+                    }});
             }
         }
-        return result.add("}").toString();
+        return Formater.getData(result, format);
     }
 }
