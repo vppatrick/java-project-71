4e768962c7a6069dc5ec488a577053640b1596f2
Vitaliy Patrikeev <vppatric@gmail.com>
2025-06-20 00:38:19 +0300

    add new condition

--- app/src/main/java/hexlet/code/Formatter.java
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -1,16 +1,18 @@
 package hexlet.code;
 
+import formatters.Json;
 import formatters.Plain;
 import formatters.Stylish;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 
 public class Formatter {
-    public static String getData(LinkedHashMap<String, HashMap<String, Object>> data, String format) {
+    public static String getData(LinkedHashMap<String, LinkedHashMap<String, Object>> data, String format) {
         if (format.equals("stylish")) {
             return Stylish.getFormat(data);
         } else if (format.equals("plain")) {
             return Plain.getFormat(data);
+        } else if (format.equals("json")) {
+            return Json.getFormat(data);
         } else {
             return "invalid format";
         }
