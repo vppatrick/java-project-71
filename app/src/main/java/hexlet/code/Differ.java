9a3a547fad1b4bb0c611af379417241b3de03221
Vitaliy Patrikeev <vppatric@gmail.com>
2025-06-20 12:55:50 +0300

    Use another way to initialize instance of Map

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -21,26 +21,26 @@ public class Differ {
             var result1 = firstMapOfData.getOrDefault(key, "absent");
             var result2 = secondMapOfData.getOrDefault(key, "absent");
             if (String.valueOf(result1).equals("absent")) { // added
-                result.put(key, new LinkedHashMap<String, Object>() {{
-                        put("state", "added");
-                        put("value", result2);
-                    }});
+                var value = new LinkedHashMap<String, Object>();
+                value.put("state", "added");
+                value.put("value", result2);
+                result.put(key, value);
             } else if (String.valueOf(result2).equals("absent")) { // removed
-                result.put(key, new LinkedHashMap<String, Object>() {{
-                        put("state", "removed");
-                        put("value", result1);
-                    }});
+                var value = new LinkedHashMap<String, Object>();
+                value.put("state", "removed");
+                value.put("value", result1);
+                result.put(key, value);
             } else if (String.valueOf(result1).equals(String.valueOf(result2))) { // no change
-                result.put(key, new LinkedHashMap<String, Object>() {{
-                        put("state", "noChange");
-                        put("value", result1);
-                    }});
+                var value = new LinkedHashMap<String, Object>();
+                value.put("state", "noChange");
+                value.put("value", result1);
+                result.put(key, value);
             } else { // updated
-                result.put(key, new LinkedHashMap<String, Object>() {{
-                        put("state", "updated");
-                        put("oldValue", result1);
-                        put("newValue", result2);
-                    }});
+                var value = new LinkedHashMap<String, Object>();
+                value.put("state", "updated");
+                value.put("oldValue", result1);
+                value.put("newValue", result2);
+                result.put(key, value);
             }
         }
         return Formatter.getData(result, format);
