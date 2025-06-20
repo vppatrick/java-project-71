c436948c28ccbd76333906d4b2587a59de7b0e43
Vitaliy Patrikeev <vppatric@gmail.com>
2025-06-20 12:29:53 +0300

    Remove public modifier

--- app/src/test/java/ParserClassTest.java
+++ app/src/test/java/ParserClassTest.java
@@ -3,15 +3,15 @@ import org.junit.jupiter.api.Test;
 import java.nio.file.Paths;
 import hexlet.code.Parser;
 
-public class ParserClassTest {
+class ParserClassTest {
     @Test
     // Тест парсинга несуществующего файла
-    public void testParserNoFile() throws Exception {
+    void testParserNoFile() throws Exception {
         var path = Paths.get("src", "test", "resources", "nofile.json")
                 .toAbsolutePath().normalize().toString();
         Exception expected = new Exception("File '" + path + "' does not exist");
         try {
-            var result = Parser.getData("src/test/resources/nofile.json");
+            Parser.getData("src/test/resources/nofile.json");
         } catch (Exception actual) {
             assertEquals(expected.getMessage(), actual.getMessage());
         }
