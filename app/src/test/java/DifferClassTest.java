23385a18ccef5fa2d988cad15f95a464c07f69a7
Vitaliy Patrikeev <vppatric@gmail.com>
2025-06-20 14:51:12 +0300

    update test

--- app/src/test/java/DifferClassTest.java
+++ app/src/test/java/DifferClassTest.java
@@ -28,90 +28,90 @@ class DifferClassTest {
     @Test
     // Сравнение двух стандартных json файлов
     void testDiffJsonNormal() throws Exception {
-        var firstJson = Parser.getData("src/test/resources/file1.json");
-        var secondJson = Parser.getData("src/test/resources/file2.json");
+        var filePath1 = "src/test/resources/file1.json";
+        var filePath2 = "src/test/resources/file2.json";
         var expected = normalizeLineEndings(readFixture("result1"));
-        var actual = normalizeLineEndings(Differ.generate(firstJson, secondJson, "stylish"));
+        var actual = normalizeLineEndings(Differ.generate(filePath1, filePath2, "stylish"));
         assertEquals(expected, actual);
     }
 
     @Test
     // Сравнение двух json файлов с одинаковыми ключами и разными значениями
     void testDiffJsonValues() throws Exception {
-        var firstJson = Parser.getData("src/test/resources/file1.json");
-        var secondJson = Parser.getData("src/test/resources/file3.json");
+        var filePath1 = "src/test/resources/file1.json";
+        var filePath2 = "src/test/resources/file3.json";
         var expected = normalizeLineEndings(readFixture("result2"));
-        var actual = normalizeLineEndings(Differ.generate(firstJson, secondJson, "stylish"));
+        var actual = normalizeLineEndings(Differ.generate(filePath1, filePath2, "stylish"));
         assertEquals(expected, actual);
     }
 
     @Test
     // Сравнение json файла с пустым файлом
     void testDiffJsonEmpty() throws Exception {
-        var firstJson = Parser.getData("src/test/resources/file1.json");
-        var secondJson = Parser.getData("src/test/resources/file4.json");
+        var filePath1 = "src/test/resources/file1.json";
+        var filePath2 = "src/test/resources/file4.json";
         var expected = normalizeLineEndings(readFixture("result3"));
-        var actual = normalizeLineEndings(Differ.generate(firstJson, secondJson, "stylish"));
+        var actual = normalizeLineEndings(Differ.generate(filePath1, filePath2, "stylish"));
         assertEquals(expected, actual);
     }
 
     @Test
     // Сравнение двух стандартных yaml файлов
     void testDiffYamlNormal() throws Exception {
-        var firstYaml = Parser.getData("src/test/resources/file1.yml");
-        var secondYaml = Parser.getData("src/test/resources/file2.yml");
+        var filePath1 = "src/test/resources/file1.yml";
+        var filePath2 = "src/test/resources/file2.yml";
         var expected = normalizeLineEndings(readFixture("result1"));
-        var actual = normalizeLineEndings(Differ.generate(firstYaml, secondYaml, "stylish"));
+        var actual = normalizeLineEndings(Differ.generate(filePath1, filePath2, "stylish"));
         assertEquals(expected, actual);
     }
 
     @Test
     // Сравнение двух yaml файлов с одинаковыми ключами и разными значениями
     void testDiffYamlValues() throws Exception {
-        var firstYaml = Parser.getData("src/test/resources/file1.yml");
-        var secondYaml = Parser.getData("src/test/resources/file3.yml");
+        var filePath1 = "src/test/resources/file1.yml";
+        var filePath2 = "src/test/resources/file3.yml";
         var expected = normalizeLineEndings(readFixture("result2"));
-        var actual = normalizeLineEndings(Differ.generate(firstYaml, secondYaml, "stylish"));
+        var actual = normalizeLineEndings(Differ.generate(filePath1, filePath2, "stylish"));
         assertEquals(expected, actual);
     }
 
     @Test
     // Сравнение yaml файла с пустым файлом
     void testDiffYamlEmpty() throws Exception {
-        var firstYaml = Parser.getData("src/test/resources/file1.yml");
-        var secondYaml = Parser.getData("src/test/resources/file4.yml");
+        var filePath1 = "src/test/resources/file1.yml";
+        var filePath2 = "src/test/resources/file4.yml";
         var expected = normalizeLineEndings(readFixture("result3"));
-        var actual = normalizeLineEndings(Differ.generate(firstYaml, secondYaml, "stylish"));
+        var actual = normalizeLineEndings(Differ.generate(filePath1, filePath2, "stylish"));
         assertEquals(expected, actual);
     }
 
     @Test
     // Сравнение двух yaml файлов с вложенными структурами в формате stylish
     void testDiffYamlNestedStylish() throws Exception {
-        var firstYaml = Parser.getData("src/test/resources/file5.yml");
-        var secondYaml = Parser.getData("src/test/resources/file6.yml");
+        var filePath1 = "src/test/resources/file5.yml";
+        var filePath2 = "src/test/resources/file6.yml";
         var expected = normalizeLineEndings(readFixture("result4"));
-        var actual = normalizeLineEndings(Differ.generate(firstYaml, secondYaml, "stylish"));
+        var actual = normalizeLineEndings(Differ.generate(filePath1, filePath2, "stylish"));
         assertEquals(expected, actual);
     }
 
     @Test
     // Сравнение двух yaml файлов с вложенными структурами в формате plain
     void testDiffYamlNestedPlain() throws Exception {
-        var firstYaml = Parser.getData("src/test/resources/file5.yml");
-        var secondYaml = Parser.getData("src/test/resources/file6.yml");
+        var filePath1 = "src/test/resources/file5.yml";
+        var filePath2 = "src/test/resources/file6.yml";
         var expected = normalizeLineEndings(readFixture("result5"));
-        var actual = normalizeLineEndings(Differ.generate(firstYaml, secondYaml, "plain"));
+        var actual = normalizeLineEndings(Differ.generate(filePath1, filePath2, "plain"));
         assertEquals(expected, actual);
     }
 
     @Test
     // Сравнение двух json файлов с вложенными структурами в формате json
     void testDiffYamlNestedJson() throws Exception {
-        var firstYaml = Parser.getData("src/test/resources/file5.yml");
-        var secondYaml = Parser.getData("src/test/resources/file6.yml");
+        var filePath1 = "src/test/resources/file5.yml";
+        var filePath2 = "src/test/resources/file6.yml";
         var expected = normalizeLineEndings(readFixture("result6"));
-        var actual = normalizeLineEndings(Differ.generate(firstYaml, secondYaml, "json"));
+        var actual = normalizeLineEndings(Differ.generate(filePath1, filePath2, "json"));
         assertEquals(expected, actual);
     }
 }
