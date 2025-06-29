587809c4a2a01fdda9f83bf25f56c2c0aa624a37
Vitaliy Patrikeev <vppatric@gmail.com>
2025-06-29 16:52:30 +0300

    forward exception to upper level

--- app/src/main/java/formatters/Json.java
+++ app/src/main/java/formatters/Json.java
@@ -1,6 +1,5 @@
 package formatters;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
