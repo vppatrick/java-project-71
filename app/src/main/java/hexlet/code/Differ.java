9cc56174bb5933379e6f2c452657190ac63bfd99
Vitaliy Patrikeev <vppatric@gmail.com>
2025-06-29 18:47:43 +0300

    update differ class

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,55 +1,13 @@
 package hexlet.code;
 
-import java.util.Map;
-import java.util.LinkedHashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Collections;
-
 public class Differ {
-    private static final String ABSENT = "absent";
-    private static final String STATE = "state";
-    private static final String VALUE = "value";
     public static String generate(String filePath1, String filePath2) throws Exception {
         return generate(filePath1, filePath2, "stylish");
     }
     public static String generate(String filePath1, String filePath2, String format) throws Exception {
-        Map<String, Object> firstMapOfData = Parser.getData(filePath1);
-        Map<String, Object> secondMapOfData = Parser.getData(filePath2);
-        var setOfKeys = new HashSet<String>();
-        setOfKeys.addAll(firstMapOfData.keySet());
-        setOfKeys.addAll(secondMapOfData.keySet());
-        List<String> sortedSetOfKeys = new ArrayList<>(setOfKeys);
-        Collections.sort(sortedSetOfKeys);
-
-        LinkedHashMap<String, LinkedHashMap<String, Object>> result = new LinkedHashMap<>();
-        for (var key : sortedSetOfKeys) {
-            var result1 = firstMapOfData.getOrDefault(key, ABSENT);
-            var result2 = secondMapOfData.getOrDefault(key, ABSENT);
-            if (String.valueOf(result1).equals(ABSENT)) { // added
-                var value = new LinkedHashMap<String, Object>();
-                value.put(STATE, "added");
-                value.put(VALUE, result2);
-                result.put(key, value);
-            } else if (String.valueOf(result2).equals(ABSENT)) { // removed
-                var value = new LinkedHashMap<String, Object>();
-                value.put(STATE, "removed");
-                value.put(VALUE, result1);
-                result.put(key, value);
-            } else if (String.valueOf(result1).equals(String.valueOf(result2))) { // no change
-                var value = new LinkedHashMap<String, Object>();
-                value.put(STATE, "noChange");
-                value.put(VALUE, result1);
-                result.put(key, value);
-            } else { // updated
-                var value = new LinkedHashMap<String, Object>();
-                value.put(STATE, "updated");
-                value.put("oldValue", result1);
-                value.put("newValue", result2);
-                result.put(key, value);
-            }
-        }
+        var firstMapOfData = Parser.getData(filePath1);
+        var secondMapOfData = Parser.getData(filePath2);
+        var result = Mapper.getDiff(firstMapOfData, secondMapOfData);
         return Formatter.getData(result, format);
     }
 }
