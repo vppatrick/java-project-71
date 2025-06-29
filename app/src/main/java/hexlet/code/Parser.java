9e85d602a06b889fe18196edf036e5afff5affb5
Vitaliy Patrikeev <vppatric@gmail.com>
2025-06-29 19:45:17 +0300

    remove file reading

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -1,9 +1,6 @@
 package hexlet.code;
 
 import java.util.Map;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -13,30 +10,15 @@ public final class Parser {
     private Parser() {
         throw new IllegalStateException("Utility class");
     }
-    public static String getFileExtension(String fileName) {
-        var dotIndex = fileName.lastIndexOf('.');
-        return dotIndex >= 0 ? fileName.substring(dotIndex + 1) : "";
-    }
-    public static Map<String, Object> getData(String filePath) throws Exception {
-        Path path = Paths.get(filePath).toAbsolutePath().normalize();
-
-        // Проверяем существование файла
-        if (!Files.exists(path)) {
-            throw new Exception("File '" + path + "' does not exist");
-        }
-
-        // Читаем файл
-        String content = Files.readString(path);
-
-        // Определяем росширение файла
-        var fileExtension = getFileExtension(filePath);
+    public static Map<String, Object> getData(String data, String format) throws Exception {
         ObjectMapper mapper = null;
-        if (fileExtension.equals("json")) {
+
+        if (format.equals("json")) {
             mapper = new ObjectMapper();
-            return mapper.readValue(content, new TypeReference<Map<String, Object>>() { });
-        } else if (fileExtension.equals("yml")) {
+            return mapper.readValue(data, new TypeReference<Map<String, Object>>() { });
+        } else if (format.equals("yml")) {
             mapper = new YAMLMapper();
-            return mapper.readValue(content, new TypeReference<Map<String, Object>>() { });
+            return mapper.readValue(data, new TypeReference<Map<String, Object>>() { });
         }
 
         return Map.of();
