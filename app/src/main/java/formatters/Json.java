fd8a3ff38a0859fc50317fbe6b173b712fc34509
Vitaliy Patrikeev <vppatric@gmail.com>
2025-06-29 23:18:08 +0300

    delete unnecessary

--- app/src/main/java/formatters/Json.java
+++ app/src/main/java/formatters/Json.java
@@ -1,33 +1,15 @@
 package formatters;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.StringJoiner;
+import hexlet.code.DiffDTO;
+import java.util.List;
 
 public final class Json {
     private Json() {
         throw new IllegalStateException("Utility class");
     }
-    public static String getFormat(Map<String, LinkedHashMap<String, Object>> data) throws Exception {
-        ObjectMapper objectMapper = new ObjectMapper();
-        StringJoiner result = new StringJoiner(",");
-        for (var entry : data.entrySet()) {
-            var key = entry.getKey();
-            var value = entry.getValue();
-            if (!value.get("state").equals("noChange")) {
-                result.add("\"" + key + "\": " + objectMapper.writeValueAsString(value));
-            }
-        }
-        return getPrintFormatJson("{" + result + "}");
-    }
-    public static String getPrintFormatJson(String data) throws Exception {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
-        JsonNode json = objectMapper.readTree(data);
-        return objectMapper.writeValueAsString(json);
+    public static String getFormat(List<DiffDTO> diffs) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsString(diffs);
     }
 }
