ed045114bc69a660c9d662357a6b367655bdb2e3
Vitaliy Patrikeev <vppatric@gmail.com>
2025-06-29 23:17:35 +0300

    changed the if construct to switch

--- app/src/main/java/hexlet/code/Formatter.java
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -3,22 +3,18 @@ package hexlet.code;
 import formatters.Json;
 import formatters.Plain;
 import formatters.Stylish;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.List;
 
 public final class Formatter {
     private Formatter() {
         throw new IllegalStateException("Utility class");
     }
-    public static String getData(Map<String, LinkedHashMap<String, Object>> data, String format) throws Exception {
-        if (format.equals("stylish")) {
-            return Stylish.getFormat(data);
-        } else if (format.equals("plain")) {
-            return Plain.getFormat(data);
-        } else if (format.equals("json")) {
-            return Json.getFormat(data);
-        } else {
-            throw new RuntimeException("Unknown format: " + format);
-        }
+    public static String getData(List<DiffDTO> data, String format) throws Exception {
+        return switch (format) {
+            case "stylish" -> Stylish.getFormat(data);
+            case "plain" -> Plain.getFormat(data);
+            case "json" -> Json.getFormat(data);
+            default -> throw new RuntimeException("Unknown format: " + format);
+        };
     }
 }
