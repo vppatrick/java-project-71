4394b8380fc9897e8e260c8e3d176aab02fe51eb
Vitaliy Patrikeev <vppatric@gmail.com>
2025-06-29 16:49:24 +0300

    throwing an exception to the upper level

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -11,22 +11,12 @@ public class Differ {
     private static final String ABSENT = "absent";
     private static final String STATE = "state";
     private static final String VALUE = "value";
-    public static String generate(String filePath1, String filePath2) {
+    public static String generate(String filePath1, String filePath2) throws Exception {
         return generate(filePath1, filePath2, "stylish");
     }
-    public static String generate(String filePath1, String filePath2, String format) {
-        Map<String, Object> firstMapOfData;
-        try {
-            firstMapOfData = Parser.getData(filePath1);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        Map<String, Object> secondMapOfData;
-        try {
-            secondMapOfData = Parser.getData(filePath2);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    public static String generate(String filePath1, String filePath2, String format) throws Exception {
+        Map<String, Object> firstMapOfData = Parser.getData(filePath1);
+        Map<String, Object> secondMapOfData = Parser.getData(filePath2);
         var setOfKeys = new HashSet<String>();
         setOfKeys.addAll(firstMapOfData.keySet());
         setOfKeys.addAll(secondMapOfData.keySet());
